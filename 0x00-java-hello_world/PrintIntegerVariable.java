034806ae6885998313b099419e5861224c6650ec
brunomanente <bruno.manente0@gmail.com>
2022-01-25 17:49:12 -0300

    variavel numero

--- 0x00-java-hello_world/PrintIntegerVariable.java
+++ 0x00-java-hello_world/PrintIntegerVariable.java
@@ -3,8 +3,9 @@ public class PrintIntegerVariable {
 
 	public static void main(String[] args) {
 		int numero = 311;
+		String avenida = "Avenida Andaló";
 		
-		System.out.println("Avenida Andaló, " + numero);
+		System.out.println(avenida + ", " + numero);
 
 	}
 
