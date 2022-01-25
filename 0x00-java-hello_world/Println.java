13e76ce9e3067a0493f19e9ea7467c8d4ae090ba
brunomanente <bruno.manente0@gmail.com>
2022-01-25 17:37:12 -0300

    println

---
+++ 0x00-java-hello_world/Println.java
@@ -0,0 +1,13 @@
+
+public class Println {
+
+	
+        public static void main(String[] args) {
+        	
+        	String texto = "Holberton School";
+        	
+            System.out.println(texto);
+        }
+    
+
+}
