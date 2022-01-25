d5893555c5b2972fa1351797b9feb4d35294148f
brunomanente <bruno.manente0@gmail.com>
2022-01-25 18:20:37 -0300

    escrevendo o valor de pi 6 linhas

--- 0x00-java-hello_world/PrintFloatVariable.java
+++ 0x00-java-hello_world/PrintFloatVariable.java
@@ -1,6 +1,6 @@
 public class PrintFloatVariable {
 	public static void main(String[] args) {
 		float pi = (float) 3.142;		
-		System.out.println("Valor de pi: " +  pi);
+		System.out.println("Valor de pi: " +  pi + '\n');
 	}
 }
