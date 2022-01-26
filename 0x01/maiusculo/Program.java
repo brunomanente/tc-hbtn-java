208bfa2a549fb4d074a37e0d733babf95395c9bf
brunomanente <bruno.manente0@gmail.com>
2022-01-26 16:55:38 -0300

    boolean

---
+++ 0x01/maiusculo/Program.java
@@ -0,0 +1,19 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		boolean caractereEhMaiusculo1 = Caractere.EhMaiusculo('a');
+        boolean caractereEhMaiusculo2 = Caractere.EhMaiusculo('B');
+        boolean caractereEhMaiusculo3 = Caractere.EhMaiusculo('d');
+        boolean caractereEhMaiusculo4 = Caractere.EhMaiusculo('f');
+        boolean caractereEhMaiusculo5 = Caractere.EhMaiusculo('G');
+
+        System.out.printf("%b\n", caractereEhMaiusculo1);
+        System.out.printf("%b\n", caractereEhMaiusculo2);
+        System.out.printf("%b\n", caractereEhMaiusculo3);
+        System.out.printf("%b\n", caractereEhMaiusculo4);
+        System.out.printf("%b\n", caractereEhMaiusculo5);
+	}
+
+}
