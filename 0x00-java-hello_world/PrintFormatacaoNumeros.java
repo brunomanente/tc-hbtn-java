2a75ac02bf64108ba191527d41c8407bf3d85a4b
brunomanente <bruno.manente0@gmail.com>
2022-01-26 09:21:29 -0300

    formatando numero float para real

--- 0x00-java-hello_world/PrintFormatacaoNumeros.java
+++ 0x00-java-hello_world/PrintFormatacaoNumeros.java
@@ -6,7 +6,7 @@ public class PrintFormatacaoNumeros {
 	public static void main(String[] args) {
 		float taxa = 0.2456f;
         float valor = 7654.321f;       
-        System.out.printf("Valor: " + String.format("R$ %, .2f",valor) + "\nTaxa: %.2f%%\n", taxa);
+        System.out.printf("Valor: " + String.format("R$ %,.2f",valor) + "\nTaxa: %.2f%%\n", taxa);
         //System.out.print(NumberFormat.getCurrencyInstance().format(valor) + '\n');
         //System.out.printf("Taxa: " + "%.2f%%", taxa);
 	}
