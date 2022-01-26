9487930f67e321ed397e715f33d44fed5a5bede1
brunomanente <bruno.manente0@gmail.com>
2022-01-26 10:54:18 -0300

    for de 0 a 99

---
+++ 0x00-java-hello_world/Numeros0a99.java
@@ -0,0 +1,17 @@
+
+public class Numeros0a99 {
+
+	public static void main(String[] args) {
+		
+		for(int i = 0; i <= 99; i++)
+		if(i <= 98)
+			{
+			System.out.print(i + ", " );
+			
+		}else {
+			System.out.println(i);
+		}
+		
+	}
+
+}
