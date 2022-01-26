b75ca04335bd3cbf4b9815dda313d2a20783b9f6
brunomanente <bruno.manente0@gmail.com>
2022-01-26 06:48:21 -0300

    utilizando concat para imprimir variaveis

---
+++ 0x00-java-hello_world/PrintStringConcat.java
@@ -0,0 +1,7 @@
+public class PrintStringConcat {
+	public static void main(String[] args) {
+		String texto1 = "Holberton";
+		String texto2 = "School";
+		System.out.print("Bem vindo a " + texto1 + " " + texto2);
+	}
+}
