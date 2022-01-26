6e2dfef41e858b3e80394b2fe01c92134cb7a51a
brunomanente <bruno.manente0@gmail.com>
2022-01-26 16:54:28 -0300

    boolean

---
+++ 0x01/maiusculo/Caractere.java
@@ -0,0 +1,13 @@
+
+public class Caractere {
+
+	public static boolean EhMaiusculo(char c) {
+		// TODO Auto-generated method stub
+		
+		if (c >= 65 && c <= 90 ){
+            return true;
+        }else
+            return false;
+    }
+}
+
