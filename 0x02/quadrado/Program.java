ff6216449aa0c115997e24131c51381d5dd779f9
brunomanente <bruno.manente0@gmail.com>
2022-01-27 14:41:01 -0300

    calculando quadrado

---
+++ 0x02/quadrado/Program.java
@@ -0,0 +1,24 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//System.out.println(Quadrado.area(5, 8));
+		try {
+            double lado1 = -5;
+            double resultado = Quadrado.area(lado1);
+            System.out.println(resultado);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+        try {
+            double lado2 = 5;
+            double resultado = Quadrado.area(lado2);
+            System.out.println(resultado);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+	}
+
+}
