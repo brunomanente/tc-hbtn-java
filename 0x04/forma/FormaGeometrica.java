c82215cfd20afb0742004fcd8a6be90679a48aa7
brunomanente <bruno.manente0@gmail.com>
2022-01-31 09:54:16 -0300

    java pacotes e herancas classes

---
+++ 0x04/forma/FormaGeometrica.java
@@ -0,0 +1,7 @@
+public class FormaGeometrica {
+	public double area() {
+		throw new UnsupportedOperationException("Funcao area nao implementada");
+		
+	}
+
+}
---
+++ 0x04/retangulo/FormaGeometrica.java
@@ -0,0 +1,9 @@
+package retangulo;
+
+public class FormaGeometrica {
+	public double area() {
+		throw new UnsupportedOperationException("Funcao area nao implementada");
+		
+	}
+
+}
