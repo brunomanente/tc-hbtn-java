210813850cec599af8904e3f1810614ae376edd1
brunomanente <bruno.manente0@gmail.com>
2022-02-01 11:49:36 -0300

    livraria

--- 0x05/livraria/Program.java
+++ 0x05/livraria/Program.java
@@ -1,7 +1,12 @@
+import java.util.Locale;
 
 public class Program {
+	
 
 	public static void main(String[] args) {
+		
+		Locale.setDefault(new Locale("pt", "BR"));
+		
 		 try {
              Livro livro1 = new Livro(
                              "As barbas do imperador: D. Pedro II",
