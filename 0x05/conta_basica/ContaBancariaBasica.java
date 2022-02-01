733225692746cbe3bc4b0836ed3634011466188d
brunomanente <bruno.manente0@gmail.com>
2022-02-01 17:14:08 -0300

    conta basica

---
+++ 0x05/conta_basica/ContaBancariaBasica.java
@@ -0,0 +1,65 @@
+import exceptions.OperacaoInvalidaException;
+
+public class ContaBancariaBasica {
+	private String numeracao;
+	private double saldo;
+	private double taxaJurosAnual;
+	
+	public ContaBancariaBasica(String numeracao, double taxaJurosAnual) {	
+		this.numeracao = numeracao;
+		this.saldo = 0;
+		this.taxaJurosAnual = taxaJurosAnual;
+	}
+	public String getNumeracao() {
+		return numeracao;
+	}
+	public double getSaldo() {
+		return saldo;
+	}
+	public double getTaxaJurosAnual() {
+		return taxaJurosAnual;
+	}
+	void depositar(double valor) throws OperacaoInvalidaException {
+		if(valor > 0) {
+			this.saldo = saldo + valor;
+		}else {
+			throw new OperacaoInvalidaException("Valor para deposito deve ser maior que 0");
+		}
+	}
+	void sacar(double valor) throws OperacaoInvalidaException {
+		/*if(valor > 0) {
+			if(saldo > valor) {
+				this.saldo = saldo - valor;		
+			}else {
+				throw new OperacaoInvalidaException("Valor de saque deve ser maior que o saldo atual");
+			}	
+		}else {
+			throw new OperacaoInvalidaException ("Valor de saque deve ser maior que 0");
+		}*/
+		if (valor < 0) {
+			throw new OperacaoInvalidaException("Valor de saque deve ser maior que 0");
+		} else if (saldo < valor) {
+			throw new OperacaoInvalidaException("Valor de saque deve ser maior que o saldo atual");
+		} else {
+			saldo -= valor;
+		}
+	
+	}	
+	public double calcularTarifaMensal() {
+		if ((saldo * 0.1) < 10) {
+			return saldo * 0.1;
+		} else {
+			return 10.00f;
+		}
+	}
+	public double calcularJurosMensal() {
+		if (saldo < 0) {
+			return 0.00f;
+		} else {
+			return saldo * ((taxaJurosAnual / 100) / 12);
+		}
+	}
+	void aplicarAtualizacaoMensal() {
+		this.saldo = saldo - calcularTarifaMensal() + calcularJurosMensal() ;
+	}	
+}
---
+++ 0x05/conta_basica/exceptions/OperacaoInvalidaException.java
@@ -0,0 +1,15 @@
+package exceptions;
+
+public class OperacaoInvalidaException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public OperacaoInvalidaException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+}
