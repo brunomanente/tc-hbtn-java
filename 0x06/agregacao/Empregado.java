9f7f324149559514232301182d416ee4ee5a4d55
brunomanente <bruno.manente0@gmail.com>
2022-02-02 13:54:49 -0300

    agregacao

--- 0x06/agregacao/Empregado.java
+++ 0x06/agregacao/Empregado.java
@@ -40,7 +40,7 @@ public class Empregado   {
 		System.out.println("Nome: " + nome);
 		System.out.println("Bairro: " + endereco.getBairro());
 		System.out.println("Cidade: " + endereco.getCidade());
-		System.out.println("Pais: " + endereco.getPais());
+		System.out.print("Pais: " + endereco.getPais());
 	}
 	
 	
