663d9bd10dd73dfcc7549fd7283c156c32c67f02
brunomanente <bruno.manente0@gmail.com>
2022-02-23 21:09:29 -0300

    predicado

--- 0x17/predicado/Program.java
+++ 0x17/predicado/Program.java
@@ -1,11 +1,10 @@
-import java.util.*;
+ import java.lang.reflect.Method;
+    import java.lang.reflect.Parameter;
+    import java.util.*;
     import java.util.function.*;
 
     public class Program {
         public static void main(String[] args) {
-        	
-        	Locale.setDefault(new Locale("pt", "BR"));
-        	
             Produto produto1 = new Produto("Cafe Chocolate Trufado Baggio",
                 21.99, 250, 120, TiposProduto.COZINHA);     
 
@@ -34,11 +33,21 @@ import java.util.*;
                 p -> p.getTipo() == TiposProduto.COZINHA);   
 
             List<Produto> produtosBaixoEstoque = ConsultaProdutos.filtrar(produtos, 
-                p -> p.getQuantidadeEmEstoque() <= 10);        
+                p -> p.getQuantidadeEmEstoque() <= 10);      
+
+            try
+            {
+                Method method = ConsultaProdutos.class.getMethod("filtrar", List.class, Predicate.class);
+                if (method != null) {
+                    System.out.println("Metodo encontrado");
+                }
+            } catch(NoSuchMethodException ex) {
+                System.out.println("Metodo nao encontrado");
+            }
 
             System.out.println(produtosPrecoMaiorQue100);
             System.out.println(produtosPesoMenorOuIgual1600);
             System.out.println(produtosCozinha);
             System.out.println(produtosBaixoEstoque);
         }
-    }
+    }    
