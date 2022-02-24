5753b7ada3acff968998ceea4e59e19cf263e3ba
brunomanente <bruno.manente0@gmail.com>
2022-02-23 21:10:50 -0300

    predicado

--- 0x17/predicado/Program.java
+++ 0x17/predicado/Program.java
@@ -5,6 +5,9 @@
 
     public class Program {
         public static void main(String[] args) {
+        	
+        	Locale.setDefault(new Locale("pt", "BR"));
+        	
             Produto produto1 = new Produto("Cafe Chocolate Trufado Baggio",
                 21.99, 250, 120, TiposProduto.COZINHA);     
 
