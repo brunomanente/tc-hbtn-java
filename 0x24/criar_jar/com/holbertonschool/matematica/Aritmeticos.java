5f8f2660b2cea924b227dbf349a527e6e9b83178
brunomanente <bruno.manente0@gmail.com>
2022-03-04 13:27:32 -0300

    script

---
+++ 0x24/criar_jar/com/holbertonschool/matematica/Aritmeticos.java
@@ -0,0 +1,17 @@
+
+public class Aritmeticos {
+
+	public int soma(int valor1, int valor2) {
+		return valor1 + valor2;
+	}
+	
+	public int subtrair(int valor1, int valor2) {
+		return valor1 - valor2;
+	}
+	public int multiplicar(int valor1, int valor2) {
+		return valor1 * valor2;
+	}
+	public int dividir(int valor1, int valor2) {
+		return valor1 / valor2;
+	}
+}
